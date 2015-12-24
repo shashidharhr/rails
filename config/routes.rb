3468d31ed062d29283c402033eae9b274833dee4
shashidharhr <shashidhar.hr@gmail.com>
2015-12-24 17:21:37 +0530

    added the insanely awesome password digest with stores the encrypted password

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'signup' => 'users#new'
 
-  get 'static_pages/help'
+  root 'static_pages#home'
 
-  get 'static_pages/about'
+  get 'help' => 'static_pages#help'
+
+  get 'about' => 'static_pages#about'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
